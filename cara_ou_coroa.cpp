5b440586d97fe9037504cf18ed8fc863760816ce
ArthurBatista117 <167575173+ArthurBatista117@users.noreply.github.com>
2024-10-12 18:27:35 -0300

    Create cara_ou_coroa

---
+++ cara_ou_coroa.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<locale>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int jogar(void);
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    enum Status {CARA, COROA};
+    Status moeda1;
+    Status moeda2;
+    int jogo;
+    
+    srand(time( 0 ));
+    jogo = jogar();
+
+    if(jogo == 0){
+        moeda1 = CARA;
+        cout << "Moeda: CARA" << endl;
+    } else{
+        moeda1 = COROA;
+        cout << "Moeda: COROA" << endl; 
+    }
+
+    jogo = jogar();
+
+    if(jogo == 0){
+        moeda2 = CARA;
+        cout << "Moeda: CARA" << endl;
+    } else{
+        moeda2 = COROA;
+        cout << "Moeda: COROA" << endl; 
+    }
+
+    if(moeda1 == moeda2){
+        cout << "Jogador ganhou!" << endl;
+    } else{
+        cout << "Jogador perdeu!" << endl;
+    }
+
+    return 0;
+}
+
+int jogar(void){
+    int jogo;
+
+    jogo = 1 + rand() % 2;
+
+    return jogo;
+}
